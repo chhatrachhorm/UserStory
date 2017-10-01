f4a354d183c81975fee6ffae09cb62cbdee4b882
Chhorm Chhatra <ch.chhatra@gmail.com>
2017-10-01 21:30:44 +0700

    Removed

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "database" : "mongodb://root:abc123@ds157624.mlab.com:57624/ioutdb?authMechanism=SCRAM-SHA-1",
-    "port" : process.env.PORT || 3000,
-    "secretKey" : "hfskljkfwhskfjnklsfjiowfjlwjfiowowjefiljiof446454)*)&(*()&*(&)fhisuhfsd"
-}
